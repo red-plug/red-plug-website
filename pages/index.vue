5721b05fb6a4276b1a1fd016cc1cf2ddaeabb8f6
andreslqr <me@andreslopez.com.mx>
2025-04-28 00:12:17 -0600

    changed og image from index redesign

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,6 @@ useSeoMeta({
     ogTitle: 'Red Plug',
     description: 'Red Plug es una organización que busca impulsar el negocio de las personas en internet, ofreciendo software modular y escalable, tu primer proyecto no tiene porque ser una limitante, te ayudamos a escalar a tu ritmo',
     ogDescription: 'Red Plug ofrece software modular y escalable, ¿quieres empezar por algo pequeño y poco a poco escalarlo? somos tu mejor opción',
-    ogImage: '/images/blog/red-plug-website/1.webp',
+    ogImage: '/images/blog/red-plug-website-2/1.webp',
 })
 </script>
