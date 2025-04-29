87820aff534678b3a1873d5312cf1f717c54b916
andreslqr <me@andreslopez.com.mx>
2025-04-28 21:36:19 -0600

    fixed us section

--- pages/nosotros.vue
+++ pages/nosotros.vue
@@ -12,18 +12,20 @@
                 (En algún lugar teníamos que aprovechar <br class="hidden md:inline-block" /> los 5 litros de agua que gastamos al<br class="hidden md:inline-block" /> generar la imagen)
               </span>
             </div>
-            <p class="mb-4">
-              Red Plug nace a partir de una idea en conjunto entre un grupo de amigos que, tras años de experiencia en el sector de la tecnología, decidieron dar un paso adelante y crear una empresa enfocada en la reutlización pero sobre todo en la
-              personalización de los productos tecnológicos (sitios y aplicaciones web, software, etc.) para cumplir con el 100% de las necesidades de sus clientes a precios accesibles.
-            </p>
-            <p class="mb-4">
-              Red Plug es una empresa joven, pero con un equipo de trabajo con más de 10 años de experiencia en el sector. Nos apasiona lo que hacemos y nos gusta pensar que somos una organización de personas que se esfuerzan por hacer las cosas bien, siempre buscando la
-              mejor solución para nuestros clientes. Creemos que cualquier negocio, de cualquier tamaña y de cualquier sector, puede beneficiarse de la tecnología y de la digitalización. Por eso, nuestro objetivo es ayudar a nuestros clientes a aprovechar al máximo las oportunidades que ofrece el mundo digital.
-            </p>
-            <p class="mb-4">
-              La organización de Red Plug esta en constante desarrollo y evolución, siempre buscando nuevas formas de mejorar nuestros servicios. Debido a nuestra filosofía <b>Plugin first, Plug later</b>, traducido del inglés como "haz el plugin ahora, úsalo después", estamos constantemente creando nuevos módulos de software para el administrador que ofrecemos
-              a nuestros clientes. Esto nos permite ofrecer un servicio de calidad y adaptado a las necesidades de cada uno, sin importar el tamaño o el sector de su negocio, con la capa de personalización que necesiten.
-            </p>
+            <div>
+              <p class="mb-4">
+                Red Plug nace a partir de una idea en conjunto entre un grupo de amigos que, tras años de experiencia en el sector de la tecnología, decidieron dar un paso adelante y crear una empresa enfocada en la reutlización pero sobre todo en la
+                personalización de los productos tecnológicos (sitios y aplicaciones web, software, etc.) para cumplir con el 100% de las necesidades de sus clientes a precios accesibles.
+              </p>
+              <p class="mb-4">
+                Red Plug es una empresa joven, pero con un equipo de trabajo con más de 10 años de experiencia en el sector. Nos apasiona lo que hacemos y nos gusta pensar que somos una organización de personas que se esfuerzan por hacer las cosas bien, siempre buscando la
+                mejor solución para nuestros clientes. Creemos que cualquier negocio, de cualquier tamaño y de cualquier sector, puede beneficiarse de la tecnología y de la digitalización. Por eso, nuestro objetivo es ayudar a nuestros clientes a aprovechar al máximo las oportunidades que ofrece el mundo digital.
+              </p>
+              <p class="mb-4">
+                La organización de Red Plug esta en constante desarrollo y evolución, siempre buscando nuevas formas de mejorar nuestros servicios. Debido a nuestra filosofía <b>Plugin first, Plug later</b>, traducido del inglés como "haz el plugin ahora, úsalo después", estamos constantemente creando nuevos módulos de software para el administrador que ofrecemos
+                a nuestros clientes. Esto nos permite ofrecer un servicio de calidad y adaptado a las necesidades de cada uno, sin importar el tamaño o el sector de su negocio, con la capa de personalización que necesiten.
+              </p>
+            </div>
           </div>
         </div>
       </div>
